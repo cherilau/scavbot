518b7982586b873fab4698a17f419a696181ce3f
cheryl-l <cheryl.lhe04@gmail.com>
2024-08-25 17:34:14 +0800

    deleted run sql file workaround

--- database.py
+++ database.py
@@ -19,7 +19,7 @@ def fetch_one(sql_statement):
 
 def fetch_many(sql_statement):
     conn = mysql.connector.connect(**config)
-
+    
     cursor = conn.cursor(dictionary=True)
     cursor.execute(sql_statement)
     result = cursor.fetchall()
@@ -37,26 +37,3 @@ def execute_sql_statement(sql_statement):
     conn.commit()
     conn.close()  
 
-
-def run_sql_file(sql_file):
-    # note: this is a WORKAROUND. only can run simple sql files
-    conn = mysql.connector.connect(**config) 
-
-    cursor = conn.cursor()
-
-    with open(sql_file, encoding="utf-8") as f:
-        commands = f.read().strip().split(';')
-
-    for command in commands:
-        cursor.execute(command)
-
-    conn.commit()
-
-    conn.close()
-
-
-# a one time thing
-# run_sql_file("sql/drop_tables.sql")
-# run_sql_file("sql/create_tables.sql")
-# run_sql_file("sql/insert_riddles.sql")
-# run_sql_file("sql/insert_groups.sql")
